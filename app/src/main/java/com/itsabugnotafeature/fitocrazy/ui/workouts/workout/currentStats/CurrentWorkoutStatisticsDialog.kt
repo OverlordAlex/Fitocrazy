333d394857ec69e90b2dadffa87a46b908d69b11
Paradise Wright <alexparadisewright+git@gmail.com>
2024-12-24 11:42:07 +0100

    Added centered bodypart chips to exercise stats dialog

--- app/src/main/java/com/itsabugnotafeature/fitocrazy/ui/workouts/workout/currentStats/CurrentWorkoutStatisticsDialog.kt
+++ app/src/main/java/com/itsabugnotafeature/fitocrazy/ui/workouts/workout/currentStats/CurrentWorkoutStatisticsDialog.kt
@@ -4,16 +4,25 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager.LayoutParams
+import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.constraintlayout.helper.widget.Flow
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.setPadding
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.itsabugnotafeature.fitocrazy.R
 import com.itsabugnotafeature.fitocrazy.ui.workouts.workout.ExerciseListViewAdapter.ExerciseView
 import kotlin.math.max
+import kotlin.reflect.typeOf
 
 class CurrentWorkoutStatisticsDialog(val exercises: List<ExerciseView>) : DialogFragment() {
 
@@ -30,7 +39,8 @@ class CurrentWorkoutStatisticsDialog(val exercises: List<ExerciseView>) : Dialog
         val baseImage = ResourcesCompat.getDrawable(resources, R.drawable.muscles_bodylines, null)
         val exercisesBySets =
             exercises.map {
-                (it.tags.joinToString(";") + ";").repeat(max(1, it.sets.size)).split(";").filterNot { it.isEmpty() } }
+                (it.tags.joinToString(";") + ";").repeat(max(1, it.sets.size)).split(";").filterNot { it.isEmpty() }
+            }
                 .flatten().groupingBy { it }.eachCount()
         val values = exercisesBySets.map { it.value }
         val min = values.min()
@@ -53,7 +63,8 @@ class CurrentWorkoutStatisticsDialog(val exercises: List<ExerciseView>) : Dialog
                     "core" -> ResourcesCompat.getDrawable(resources, R.drawable.muscles_core, null)
                     else -> ResourcesCompat.getDrawable(resources, R.drawable.muscles_bodylines, null)
                 }.also { drawable ->
-                    drawable?.alpha = if (it.value <= lowerBoundary) leastAlpha else if (it.value <= upperBoundary) mediumAlpha else mostAlpha
+                    drawable?.alpha =
+                        if (it.value <= lowerBoundary) leastAlpha else if (it.value <= upperBoundary) mediumAlpha else mostAlpha
                     Log.i("TEST", "${it.key} has ${it.value} and so has ${drawable?.alpha}")
                 }
             }.toTypedArray(),
@@ -66,20 +77,25 @@ class CurrentWorkoutStatisticsDialog(val exercises: List<ExerciseView>) : Dialog
 
         dialog?.setCanceledOnTouchOutside(true)
         dialog?.window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+
+        val parent = view.findViewById<ConstraintLayout>(R.id.layout_parent)
+        val chipGroup = view.findViewById<Flow>(R.id.chipgroup_Bodyparts2)
+        exercises.map { it.tags }.flatten().toSet().forEach { chipName ->
+            val newChip = Chip(chipGroup.context)
+            newChip.text = chipName
+            newChip.id = View.generateViewId()
+            newChip.setChipBackgroundColorResource(R.color.blue_accent_light)
+//            newChip.layoutParams = FrameLayout.LayoutParams(
+//                FrameLayout.LayoutParams.WRAP_CONTENT,
+//                FrameLayout.LayoutParams.WRAP_CONTENT,
+//                Gravity.END
+//            )
+            parent.addView(newChip)
+            chipGroup.addView(newChip)
+        }
+        chipGroup.invalidate()
+        chipGroup.requestLayout()
     }
-
-
-    /* A secret tool for later (populating the chip group)
-    private fun getTopTags(): String {
-        return dataList
-            .fold(mutableListOf<String>()) { ongoing, item -> repeat(item.sets.size) { ongoing.addAll(item.tags) }; ongoing }
-            .groupingBy { it }
-            .eachCount().asIterable()
-            .sortedBy { it.value }.reversed()
-            .take(3)
-            .joinToString(" ") { it.key }
-            .trim()
-    }*/
 
     companion object {
         const val TAG = "CurrentWorkoutStatisticsDialog"
